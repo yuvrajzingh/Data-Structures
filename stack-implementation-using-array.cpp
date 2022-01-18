dcfea37e152bb26c461658cb287d8090d93f0eb8
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-01-18 23:09:09 +0530

    Update stack-implementation-using-array.cpp

--- stack-implementation-using-array.cpp
+++ stack-implementation-using-array.cpp
@@ -1,7 +1,5 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-//to check if a stack is empty or not
 
 class stack
 {
@@ -22,9 +20,10 @@ int isEmpty(stack *ptr)
         return 0;
     }
 }
+
 int isFull(stack *ptr)
 {
-    if (ptr->top == ptr->size-1)
+    if (ptr->top == ptr->size - 1)
     {
         return 1;
     }
@@ -33,22 +32,68 @@ int isFull(stack *ptr)
         return 0;
     }
 }
-int main()
-{
-    class stack *s = new stack();
-    s->size = 80;
-    s->top = -1;
-    s->arr = new int(s->size);
 
-    if(isEmpty(s))
+int pop(stack *ptr)
+{
+    if (isEmpty(ptr))
     {
-        cout<<"The stack is Empty";
+        cout << "Stack Underflow";
     }
     else
     {
-        cout<<"The stack is not Empty";
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
     }
+}
 
+void push(stack *ptr, int val)
+{
+    if (isFull(ptr))
+    {
+        cout << "Stack overflow";
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+
+int peek(class stack *s, int i)
+{
+    int arrayInd = s->top - i + 1;
+    if (arrayInd < 0)
+    {
+        cout << "Not a valid position for the stack\n";
+        return -1;
+    }
+    else
+    {
+        return s->arr[arrayInd];
+    }
+}
+
+int main()
+{
+    class stack *s = new stack();
+    s->size = 15;
+    s->top = -1;
+    s->arr = new int(s->size);
+
+    cout << "Stack has been created successfully\n";
+
+    push(s, 10);
+    push(s, 5);
+    push(s, 1);
+    push(s, 40);
+
+    cout<<"Peeking :"<<endl;
+    
+    for (int j = 1; j <= s->top + 1; j++)
+    {
+        printf("The value at position %d is %d\n", j, peek(s, j));
+    }
 
     return 0;
 }
