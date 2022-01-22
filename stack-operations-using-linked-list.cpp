087d25a5bd76220eccf78f67b9df0f39e387181a
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-01-22 23:53:35 +0530

    Update stack-operations-using-linked-list.cpp

--- stack-operations-using-linked-list.cpp
+++ stack-operations-using-linked-list.cpp
@@ -1,71 +1,117 @@
 #include <iostream>
 using namespace std;
-class Node {
-    public:
-        int data;
-        Node *next;
+
+
+class Node
+{
+public:
+   int data;
+   Node *next;
 };
-class Node* top = NULL;
-void push(int val) {
-   class Node* newnode = new Node();
+
+
+class Node *top = NULL;
+void push(int val)
+{
+   class Node *newnode = new Node();
    newnode->data = val;
    newnode->next = top;
    top = newnode;
 }
-void pop() {
-   if(top==NULL)
-   cout<<"Stack Underflow"<<endl;
-   else {
-      cout<<"The popped element is "<< top->data <<endl;
+void pop()
+{
+   if (top == NULL)
+      cout << "Stack Underflow" << endl;
+   else
+   {  
+      cout << "The popped element is " << top->data << endl;
       top = top->next;
    }
 }
-void display() {
-   class Node* ptr;
-   if(top==NULL)
-   cout<<"stack is empty";
-   else {
+
+int peek(int pos)
+{
+   
+   class Node * ptr = top;
+   for(int i = 0;i < pos-1 && ptr!=NULL; i++)
+   {
+      ptr = ptr->next;
+   }
+   if(ptr!=NULL)
+   {
+      return ptr->data;
+   }
+   else
+   {
+      return -1;
+   }
+}
+
+void display()
+{
+   class Node *ptr;
+   if (top == NULL)
+      cout << "stack is empty";
+   else
+   {
       ptr = top;
-      cout<<"Stack elements are: ";
-      while (ptr != NULL) {
-         cout<< ptr->data <<" ";
+      cout << "Stack elements are: ";
+      while (ptr != NULL)
+      {
+         cout << ptr->data << " ";
          ptr = ptr->next;
       }
    }
-   cout<<endl;
+   cout << endl;
 }
-int main() {
-   int ch, val;
-   cout<<"1) Push in stack"<<endl;
-   cout<<"2) Pop from stack"<<endl;
-   cout<<"3) Display stack"<<endl;
-   cout<<"4) Exit"<<endl;
-   do {
-      cout<<"Enter choice: "<<endl;
-      cin>>ch;
-      switch(ch) {
-         case 1: {
-            cout<<"Enter value to be pushed:"<<endl;
-            cin>>val;
-            push(val);
-            break;
-         }
-         case 2: {
-            pop();
-            break;
-         }
-         case 3: {
-            display();
-            break;
-         }
-         case 4: {
-            cout<<"Exit"<<endl;
-            break;
-         }
-         default: {
-            cout<<"Invalid Choice"<<endl;
-         }
+int main()
+{
+   int ch, val, pos;
+   cout << "1) Push in stack" << endl;
+   cout << "2) Pop from stack" << endl;
+   cout << "3) Peek at any position in stack" << endl;
+   cout << "4) Display stack" << endl;
+   cout << "5) Exit" << endl;
+   do
+   {
+      cout << "Enter choice: " << endl;
+      cin >> ch;
+      switch (ch)
+      {
+      case 1:
+      {
+         cout << "Enter value to be pushed:" << endl;
+         cin >> val;
+         push(val);
+         break;
       }
-   }while(ch!=4);
+      case 2:
+      {
+         pop();
+         break;
+      }
+      case 3:
+      {
+         cout<<"Enter the index at which you want to peek:"<<endl;
+         cin >> pos;
+         cout<<"Peak function called - Value at postion "<<pos<<" is "<<peek(pos)<<endl;
+         break;
+      }
+      case 4:
+      {
+         display();
+         break;
+      }
+      case 5:
+      {
+         cout << "Exit" << endl;
+         break;
+      }
+      default:
+      {
+         cout << "Invalid Choice" << endl;
+      }
+      }
+   } while (ch != 5);
    return 0;
 }
