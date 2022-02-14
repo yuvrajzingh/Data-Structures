f6b9d9fc22a078b4db644e515df742d3ba696631
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-02-14 23:53:11 +0530

    Add files via upload

---
+++ record-breaking.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    int a[n+1];
+    a[n] = -1;
+
+    cout<<"Enter the elements: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+    if(n==1)
+    {
+        cout<<"1"<<endl;
+        return 0;
+    }
+
+    int ans = 0;
+    int mx = -1;
+
+    for(int i=0; i<n; i++)
+    {
+        if(a[i]<mx && a[i]>a[i+1])
+        {
+            ans++;
+        }
+        mx = max(mx, a[i]);
+    }
+    cout<<"The number of record breaking day is : ";
+    cout<<ans<<endl;
+
+    return 0;
+}
