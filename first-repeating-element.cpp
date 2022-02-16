6a359c84a81f66609aab6ec0367ab0de8813583f
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-02-16 22:22:32 +0530

    Add files via upload

---
+++ first-repeating-element.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//returning position of the element that repeats itself and is the smallest
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    int a[n];
+    cout<<"Enter the elements: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+    const int N = 1e6+2;
+    int idx[N];
+
+    for(int i=0; i<N; i++)
+    {
+        idx[i] = -1;
+    }
+    
+    int minidx = INT_MAX;
+
+    for(int i=0; i<n ; i++)
+    {
+        if(idx[a[i]] != -1)
+        {
+            minidx = min(minidx, idx[a[i]]);
+        }
+        else
+        {
+            idx[a[i]] = i;
+        }
+    }
+
+    if(minidx == INT_MAX)
+    {
+        cout<<"-1"<<endl;
+    }
+    else
+    {
+        cout<<minidx + 1<<endl;
+    }
+
+    return 0;
+}
