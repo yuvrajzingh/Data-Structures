bbdbb7c4c78b43bff8f20d5f360e97d750437f28
Yuvraj Singh <88030872+yuvrajzingh@users.noreply.github.com>
2022-04-15 23:45:23 +0530

    Add files via upload

---
+++ recursion-check-if-array-is-sorted.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool sorted(int arr[], int n)
+{
+    if(n==1)
+    {
+        return true;
+    }
+
+    bool restArray = sorted(arr+1, n-1);
+    return (arr[0]<arr[1] && restArray);
+}
+
+
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+
+    cout<<"1 for sorted and 0 for unsorted: "<<sorted(arr,5)<<endl;
+
+    return 0;
+}
